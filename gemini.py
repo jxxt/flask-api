f41d3eaf889abe7b4c99aa2de7b8678a12f0e825
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-12-17 09:17:13 +0530

    set up gemini

--- gemini.py
+++ gemini.py
@@ -1,9 +1,13 @@
 import os
+import google.generativeai as genai
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 apiKey = os.getenv("API_KEY")
 
+genai.configure(api_key=apiKey)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-
+response = model.generate_content("Explain how AI works in one sentence")
+print("\n\n",response.text,"\n")
