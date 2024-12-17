cce6206e7e48787f29d015939d4ef8d739ed676c
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-12-17 11:02:58 +0530

    added test fastapi

--- gemini.py
+++ gemini.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
-
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 
 apiKey = os.getenv("API_KEY")
---
+++ testfastapi.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv, find_dotenv
+
+app = FastAPI()
+
+@app.post("/")
+async def query(q: str):
+    
+    load_dotenv(find_dotenv())
+
+    apiKey = os.getenv("API_KEY")
+
+    genai.configure(api_key=apiKey)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
+    response = model.generate_content(q)
+    # print("\n\n",response.text,"\n")
+    
+    return {"message": response.text}
+
+
+
+
+
+# @app.get("/")
+# async def root():
+    
+#     load_dotenv(find_dotenv())
+
+#     apiKey = os.getenv("API_KEY")
+
+#     genai.configure(api_key=apiKey)
+#     model = genai.GenerativeModel("gemini-1.5-flash")
+
+#     response = model.generate_content("Explain how AI works in one sentence")
+#     # print("\n\n",response.text,"\n")
+    
+#     return {"message": response.text}
